66b4db4211c0e7c30b2179ed16d0e0e9217f7412
Howie Wang <howiewang18@gmail.com>
2020-09-21 00:25:08 +0000

    Optimize at O3.

--- stack_examination/test.s
+++ stack_examination/test.s
@@ -6,30 +6,7 @@
 a:                                      # @a
 	.cfi_startproc
 # %bb.0:
-	pushq	%rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset %rbp, -16
-	movq	%rsp, %rbp
-	.cfi_def_cfa_register %rbp
-	subq	$16, %rsp
-	movl	%edi, -8(%rbp)
-	cmpl	$0, -8(%rbp)
-	jle	.LBB0_2
-# %bb.1:
-	movl	-8(%rbp), %eax
-	addl	$-1, %eax
-	movl	%eax, -8(%rbp)
-	movl	%eax, %edi
-	callq	a
-	movl	%eax, -4(%rbp)
-	jmp	.LBB0_3
-.LBB0_2:
-	movl	$0, -4(%rbp)
-.LBB0_3:
-	movl	-4(%rbp), %eax
-	addq	$16, %rsp
-	popq	%rbp
-	.cfi_def_cfa %rsp, 8
+	xorl	%eax, %eax
 	retq
 .Lfunc_end0:
 	.size	a, .Lfunc_end0-a
@@ -41,18 +18,7 @@ a:                                      # @a
 main:                                   # @main
 	.cfi_startproc
 # %bb.0:
-	pushq	%rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset %rbp, -16
-	movq	%rsp, %rbp
-	.cfi_def_cfa_register %rbp
-	subq	$16, %rsp
-	movl	$0, -4(%rbp)
-	movl	$1, %edi
-	callq	a
-	addq	$16, %rsp
-	popq	%rbp
-	.cfi_def_cfa %rsp, 8
+	xorl	%eax, %eax
 	retq
 .Lfunc_end1:
 	.size	main, .Lfunc_end1-main
@@ -62,5 +28,3 @@ main:                                   # @main
 	.ident	"clang version 7.0.1-8 (tags/RELEASE_701/final)"
 	.section	".note.GNU-stack","",@progbits
 	.addrsig
-	.addrsig_sym a
-	.addrsig_sym main
